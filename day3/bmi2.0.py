3abbf808f5b8a1093eb573244945026063b5aeab
Ujwal KC <ujwalkcsps@gmail.com>
2023-06-10 21:36:23 +0530

    simple changes

--- day3/bmi2.0.py
+++ day3/bmi2.0.py
@@ -8,5 +8,9 @@ if bmi < 18.5:
     print("You are underweight")
 elif bmi < 25:
     print("You are normal weight")
-else:
+elif bmi < 30:
     print("You are overweight")
+elif bmi < 35:
+    print("You are obese")
+else:
+    print("You are clinical obese")
