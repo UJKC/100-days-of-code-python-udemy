750b2081fdc02301990d308fc75e2e79170a6605
Ujwal KC <ujwalkcsps@gmail.com>
2023-06-11 21:36:32 +0530

    day 5 add even number python code

--- day5/README.md
+++ day5/README.md
@@ -1,3 +1,5 @@
 1. Average height
 
 2. Highest Score
+
+3. Add even number from 2 to 100
---
+++ day5/addeven.py
@@ -0,0 +1,5 @@
+print("Add all even number from 2 to 100")
+total = 0
+for add in range(2, 101, 2):
+    total += add
+print(f"Total is: {total}")
