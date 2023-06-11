6b3b4e744c93b98fccc3e9e7ddb3003b273e0d9b
Ujwal KC <ujwalkcsps@gmail.com>
2023-06-11 21:43:55 +0530

    day 5 fizzbuzz game python code

--- day5/README.md
+++ day5/README.md
@@ -3,3 +3,9 @@
 2. Highest Score
 
 3. Add even number from 2 to 100
+
+4. FizzBuzz
+Conditions:
+    1. If a number is divisible by 3 say "FIZZ"
+    2. If a number is divisible by 5 say "BUZZ"
+    3. If a number is divisible by 3 and 5 say "FIZZBUZZ"
---
+++ day5/fizzbuzz.py
@@ -0,0 +1,10 @@
+print("FizzBuzz Game")
+for number in range(1, 100):
+    if number % 3 == 0 and number % 5 != 0:
+        print("FIZZ")
+    elif number % 5 == 0 and number % 3 != 0:
+        print("BUZZ")
+    elif number % 3 == 0 and number % 5 == 0:
+        print("FIZZBUZZ")
+    else:
+        print(number)
