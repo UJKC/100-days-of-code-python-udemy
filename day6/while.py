63317a74809baaf2ebc72f3a6e37fa436aad697c
Ujwal KC <ujwalkcsps@gmail.com>
2023-06-13 19:31:07 +0530

    day 6 sum python code

---
+++ day6/while.py
@@ -0,0 +1,8 @@
+print("Implementation of while loop")
+print("Add add numbers from 100 till 0")
+sum = 0
+initial = 100
+while initial > 0:
+    sum += initial
+    initial -= 1
+print(f"Sum is {sum}")
