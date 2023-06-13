e32cc0303ab5b0d11fe2c25a4b732e17ce779140
Ujwal KC <ujwalkcsps@gmail.com>
2023-06-14 00:30:21 +0530

    changes with ceaser

--- day8/ceaser.py
+++ day8/ceaser.py
@@ -1,8 +1,8 @@
-alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s" ,"t", "u", "v", "w", "x", "v", "z"]
-safe = []
-desafe = []
-condition = input("Ehat do you want to do? Encrypt or Decrypt: ").lower()
-if condition == "encrypt":
+def initial():
+    condition = input("What do you want to do? Encrypt or Decrypt: ").lower()
+    condition1(condition)
+
+def encrypt():
     string = input("Enter the word to be encrypted: ")
     shift = int(input("Enter the number of place the number has to be shifted: "))
     for i in string:
@@ -14,7 +14,9 @@ if condition == "encrypt":
         after = alpha[j + shift]
         safe.append(after)
     print(safe)
-else:
+    initial()
+
+def decrypt():
     string = input("Enter the word to be encrypted: ")
     shift = int(input("Enter the number of place the number has to be shifted: "))
     for i in string:
@@ -23,6 +25,18 @@ else:
             if i == alpha[j]:
                 break
             j += 1
-        after = alpha[j - shift]
-        safe.append(after)
-    print(safe)
+        after1 = alpha[j - shift]
+        desafe.append(after1)
+    print(desafe)
+    initial()
+
+def condition1(condition):
+    if condition == "encrypt":
+        encrypt()
+    else:
+        decrypt()
+
+alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s" ,"t", "u", "v", "w", "x", "v", "z"]
+safe = []
+desafe = []
+initial()
