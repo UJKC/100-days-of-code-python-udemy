1e9fe73d0f6434bce53acd71660a35d46e3d3395
Ujwal KC <ujwalkcsps@gmail.com>
2023-06-15 17:29:15 +0530

    day 13 debugging leap year python code

--- day13/leap.py
+++ day13/leap.py
@@ -4,12 +4,10 @@
 #year = int(input("Enter the input year: "))
 #if year % 4 == 0:
 #    if year % 100 == 0:
-#        if year % 400 == 0:
+#    if year % 400 == 0:
 #            print("Year is Leap year")
 #        else:
 #            print("Year is not leap year")
-#    else:
-#        print("Year is leap year")
 #else:
 #    print("Year is not leap year")
 
