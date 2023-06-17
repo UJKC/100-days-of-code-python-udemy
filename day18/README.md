ee66c23178b164d9e4e7601b5785d7672bb7e724
Ujwal KC <ujwalkcsps@gmail.com>
2023-06-17 13:24:04 +0530

    day 18 turtle shapes python code

--- day18/README.md
+++ day18/README.md
@@ -1,3 +1,5 @@
 1. Turtle Square
 
 2. Dotted Line
+
+3. Triangle, Square, Pentagon, Hexagon, Heptagon, Octagon, Nonagon, Decagon
---
+++ day18/shapes.py
@@ -0,0 +1,29 @@
+from turtle import Turtle, Screen
+
+cursor = Turtle()
+for i in range(1, 4):
+    cursor.forward(10)
+    cursor.left(120)
+for i in range(1, 5):
+    cursor.forward(20)
+    cursor.left(90)
+for i in range(1, 6):
+    cursor.forward(30)
+    cursor.left(72)
+for i in range(1, 7):
+    cursor.forward(40)
+    cursor.left(60)
+for i in range(1, 8):
+    cursor.forward(50)
+    cursor.left(51.5)
+for i in range(1, 9):
+    cursor.forward(60)
+    cursor.left(45)
+for i in range(1, 10):
+    cursor.forward(70)
+    cursor.left(40)
+for i in range(1, 11):
+    cursor.forward(80)
+    cursor.left(36)
+screen = Screen()
+screen.exitonclick()
