ea3f9369dba7583b024bb72aeabf19d79809de82
Ujwal KC <ujwalkcsps@gmail.com>
2023-07-09 00:39:05 +0530

    day 30 nato lettes python code

--- day30/main.py
+++ day30/main.py
@@ -3,13 +3,18 @@
 
 import pandas
 
-data = pandas.read_csv("nato_phonetic_alphabet.csv")
-#TODO 1. Create a dictionary in this format:
-phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
-print(phonetic_dict)
+data = pandas.read_csv("day30\\nato_phonetic_alphabet.csv")
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+def solve():
+    phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 
-word = input("Enter a word: ").upper()
-output_list = [phonetic_dict[letter] for letter in word]
-print(output_list)
+    try:
+        word = input("Enter a word: ").upper()
+        output_list = [phonetic_dict[letter] for letter in word]
+        print(output_list)
+    
+    except KeyError:
+        print("Ask for letters not numbers")
+        solve()
+    
+solve()
