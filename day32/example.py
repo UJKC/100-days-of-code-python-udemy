5aa400945fb62461055b920e99f5b2cd04abb0c0
Ujwal KC <ujwalkcsps@gmail.com>
2023-07-12 19:01:07 +0530

    day 32 example SMTP python code

---
+++ day32/example.py
@@ -0,0 +1,9 @@
+import smtplib
+
+my_email = "ujwalkcsps@gmail.com"
+password = ""
+
+with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+    connection.starttls()
+    connection.login(user=my_email, password=password)
+    connection.sendmail(from_addr=my_email, to_addrs="", msg="Subject:Hello\n\nI am ujwal")
