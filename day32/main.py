c0a694dafe4c3b71b3b2d3ca0497b83e3a62d878
Ujwal KC <ujwalkcsps@gmail.com>
2023-07-13 21:08:30 +0530

    day 32 happy Birthday Wishes python code

--- day32/main.py
+++ day32/main.py
@@ -11,3 +11,36 @@
 
 
 
+import pandas
+import datetime as dt
+import smtplib
+import random
+
+PLACE_HOLDER = "[NAME]"
+
+with open("day32\\birthdays.csv")as birth:
+    data = pandas.read_csv(birth)
+
+need = dt.datetime.now()
+for (index, row) in data.iterrows():
+    if (row['year'] == need.year) and (row['month'] == need.month) and (row['day'] == need.day):
+
+        random_letter = random.randint(1, 3)
+        if random_letter == 1:
+            with open("day32\\letter_templates\\letter_1.txt") as bd:
+                happy = bd.read()
+        elif random_letter == 2:
+            with open("day32\\letter_templates\\letter_2.txt") as bd:
+                happy = bd.read()
+        else:
+            with open("day32\\letter_templates\\letter_3.txt") as bd:
+                happy = bd.read()
+        hbd = happy.replace(PLACE_HOLDER, row["name"])
+
+
+        my_email = "ujwalkcsps@gmail.com"
+        password = ""
+        with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+            connection.starttls()
+            connection.login(user=my_email, password=password)
+            connection.sendmail(from_addr=my_email, to_addrs="", msg=f"Subject:Birthday Wishes\n\n{hbd}")
