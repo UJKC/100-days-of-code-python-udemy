5a496b8b41898f7bbf3efc18d02dcc8a9e0caf4e
Ujwal KC <ujwalkcsps@gmail.com>
2023-07-23 09:39:41 +0530

    kanye quotes python code

--- day33/main.py
+++ day33/main.py
@@ -1,9 +1,14 @@
 from tkinter import *
+import requests
 
 
 def get_quote():
-    pass
-    #Write your code here.
+    response = requests.get(url="https://api.kanye.rest/")
+    response.raise_for_status()
+
+    quotes_api = response.json()
+    quotes = quotes_api["quote"]
+    canvas.itemconfig(quote_text, text=quotes)
 
 
 
@@ -12,12 +17,12 @@ window.title("Kanye Says...")
 window.config(padx=50, pady=50)
 
 canvas = Canvas(width=300, height=414)
-background_img = PhotoImage(file="background.png")
+background_img = PhotoImage(file="day33\\background.png")
 canvas.create_image(150, 207, image=background_img)
 quote_text = canvas.create_text(150, 207, text="Kanye Quote Goes HERE", width=250, font=("Arial", 30, "bold"), fill="white")
 canvas.grid(row=0, column=0)
 
-kanye_img = PhotoImage(file="kanye.png")
+kanye_img = PhotoImage(file="day33\\kanye.png")
 kanye_button = Button(image=kanye_img, highlightthickness=0, command=get_quote)
 kanye_button.grid(row=1, column=0)
 
