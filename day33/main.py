87caecb7761b9984d54623f3d562182a77769294
Ujwal KC <ujwalkcsps@gmail.com>
2023-07-23 10:59:41 +0530

    day 33 iss overhead python code

--- day33/main.py
+++ day33/main.py
@@ -1,8 +1,11 @@
 import requests
 from datetime import datetime
+import smtplib
+import time
 
 MY_LAT = 51.507351 # Your latitude
 MY_LONG = -0.127758 # Your longitude
+status = 0
 
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
 response.raise_for_status()
@@ -12,6 +15,10 @@ iss_latitude = float(data["iss_position"]["latitude"])
 iss_longitude = float(data["iss_position"]["longitude"])
 
 #Your position is within +5 or -5 degrees of the ISS position.
+
+if (iss_latitude >= (MY_LAT - 5)) and (iss_latitude <= (MY_LAT + 5)):
+    if (iss_longitude >= (MY_LONG - 5)) and (iss_longitude <= (MY_LONG + 5)):
+        status = 1
 
 
 parameters = {
@@ -34,4 +41,13 @@ time_now = datetime.now()
 # BONUS: run the code every 60 seconds.
 
 
-
+while True:
+    time.sleep(100)
+    if sunset <= time_now.hour and sunrise >= time_now.hour :
+        if status == 1:
+            my_email = "ujwalkcsps@gmail.com"
+            password = "9448906153"
+            with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+                connection.starttls()
+                connection.login(user=my_email, password=password)
+                connection.sendmail(from_addr=my_email, to_addrs="udaikcsps@gmail.com", msg=f"Subject:Look Up\n\nISS Over You")
