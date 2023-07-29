287e6694ee5c251855f4f7ba0cfb1096203c037e
Ujwal KC <ujwalkcsps@gmail.com>
2023-07-29 08:32:22 +0530

    day 37 update the value on particular date python

--- day37/main.py
+++ day37/main.py
@@ -32,8 +32,15 @@ date_today = datetime.datetime.now()
 
 parameter_add = {
     #curl -X POST https://pixe.la/v1/users/a-know/graphs/test-graph -H 'X-USER-TOKEN:thisissecret' -d '{"date":"20180915","quantity":"5"}'
-    "date": "",
+    "date": date_today.strftime('%Y%m%d'),
     "quantity":"3"
+}
+
+url_graph_endpoint_update = f"https://pixe.la/v1/users/{parameter_post['username']}/graphs/{parameter_graph['id']}/{date_today.strftime('%Y%m%d')}"
+
+parameter_update = {
+    #curl -X PUT https://pixe.la/v1/users/a-know/graphs/test-graph/20180915 -H 'X-USER-TOKEN:thisissecret' -d '{"quantity":"7","optionalData":"{\"key\":\"value\"}"}'
+    "quantity":"7",
 }
 
 #'X-USER-TOKEN:thisissecret'
@@ -52,6 +59,13 @@ parameter_add = {
 
 #Add a value on particular date
 #response = requests.post(url= url_graph_endpoint_add, json=parameter_add, headers=headers)
+#response.raise_for_status()
+#print(response.status_code)
+#print(response.json())
+
+
+#Update the value on particular date
+#response = requests.put(url= url_graph_endpoint_update, json=parameter_update, headers=headers)
 #response.raise_for_status()
 #print(response.status_code)
 #print(response.json())
