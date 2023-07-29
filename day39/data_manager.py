96b1f25569ded7e60f7a5da182ae9fafe5ccf21c
Ujwal KC <ujwalkcsps@gmail.com>
2023-07-29 23:15:54 +0530

    day 39 incomplete project

--- day39/data_manager.py
+++ day39/data_manager.py
@@ -1,3 +1,16 @@
+import requests
+
+Sheety_endpoint = "https://api.sheety.co/22d264e8ad79207275125e9853aeb27f/copyOfFlightDeals/prices"
+
 class DataManager:
     #This class is responsible for talking to the Google Sheet.
-    pass
+    
+    def __init__(self):
+        self.destination_data = []
+
+    def get_destination_data(self):
+        self.response_destination_data = requests.get(url=Sheety_endpoint)
+        return self.response_destination_data
+    
+    # {'prices': [{'city': 'Paris', 'iataCode': 'CDG', 'lowestPrice': 54, 'id': 2}, {'city': 'Berlin', 'iataCode': 'SXF', 'lowestPrice': 42, 'id': 3}, {'city': 'Tokyo', 'iataCode': 'HND', 'lowestPrice': 485, 'id': 4}, {'city': 'Sydney', 'iataCode': 'MCY', 'lowestPrice': 551, 'id': 5}, {'city': 'Istanbul', 'iataCode': 'IST', 'lowestPrice': 95, 'id': 6}, {'city': 'Kuala Lumpur', 'iataCode': 'KUL', 'lowestPrice': 414, 'id': 7}, {'city': 'New York', 'iataCode': 'JFK', 'lowestPrice': 240, 'id': 8}, {'city': 'San Francisco', 'iataCode': 'SFO', 'lowestPrice': 260, 'id': 9}, {'city': 'Cape Town', 'iataCode': 'NLP', 'lowestPrice': 378, 'id': 10}, 
+    # {'city': 'Bombay', 'iataCode': 'BOM', 'lowestPrice': 400, 'id': 11}]}
--- day39/main.py
+++ day39/main.py
@@ -1,1 +1,7 @@
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+
+from data_manager import DataManager as DM
+
+sheety_requests = DM()
+data_response_got_from_sheety = sheety_requests.get_destination_data()
+print(data_response_got_from_sheety.json())
