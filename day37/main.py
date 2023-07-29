5ea6c52a1e13a7e5ca5c2f098f0764af1b23f248
Ujwal KC <ujwalkcsps@gmail.com>
2023-07-29 08:50:21 +0530

    day 37 pixela api python code

--- day37/main.py
+++ day37/main.py
@@ -5,8 +5,8 @@ url_user_endpoint = "https://pixe.la/v1/users"
 
 parameter_post = {
     #curl -X POST https://pixe.la/v1/users -d '{"token":"thisissecret", "username":"a-know", "agreeTermsOfService":"yes", "notMinor":"yes"}'
-    "token": "ujwalkcspsgmailcom",
-    "username": "ujwal",
+    "token": "",
+    "username": "",
     "agreeTermsOfService": "yes",
     "notMinor": "yes"
 }
@@ -77,6 +77,12 @@ parameter_update = {
 
 #Delete a graph
 #response = requests.delete(url= url_graph_endpoint_add, headers=headers)
+#response.raise_for_status()
+#print(response.status_code)
+#print(response.json())
+
+#Delete a user
+#response = requests.delete(url= url_graph_endpoint, headers=headers)
 #response.raise_for_status()
 #print(response.status_code)
 #print(response.json())
