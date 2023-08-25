8a99dba58fbf44c0e9d582d0af84a1571b57a8e1
Ujwal KC <ujwalkcsps@gmail.com>
2023-08-25 21:38:39 +0530

    day 45 web scrapping python code

--- day45/main.py
+++ day45/main.py
@@ -11,3 +11,25 @@ print(Soup.title.name)
 print(Soup.title.string)
 print(Soup.a)
 print(Soup.prettify())
+
+all_anchor_tag = Soup.find_all(name="a")
+print(all_anchor_tag)
+
+for tag in all_anchor_tag:
+    print(tag.getText())
+    print(tag.get("href"))
+
+heading = Soup.find(name="h1", id='name')
+print(heading)
+print(heading.getText())
+
+heading_class = Soup.find(name="h3", class_="heading")
+print(heading)
+print(heading.getText())
+
+selector = Soup.select(selector="p a")
+print(selector)
+
+for tag in selector:
+    print(tag.get("href"))
+    print(tag.string)
