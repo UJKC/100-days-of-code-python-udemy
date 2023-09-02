df6660691521d77d571c41751768e10f7c4a1d1f
Ujwal KC <ujwalkcsps@gmail.com>
2023-09-02 15:11:27 +0530

    day 48 Entering the keys python code

--- day48/wiki.py
+++ day48/wiki.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
@@ -9,6 +10,8 @@ driver.get("https://en.wikipedia.org/wiki/Main_Page")
 
 arti_count = driver.find_element(By.CSS_SELECTOR, value="#articlecount a")
 print(arti_count.text)
+#arti_count.click()
 
-driver.close()
-driver.quit()
+portals = driver.find_element(By.NAME, value= "search")
+portals.send_keys("Python")
+portals.send_keys(Keys.ENTER)
