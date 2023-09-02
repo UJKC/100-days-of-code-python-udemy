3f33d2d8616c022b140a6174c014404ea7b45283
Ujwal KC <ujwalkcsps@gmail.com>
2023-09-02 13:17:57 +0530

    day 48 alternative to day 47

--- day48/example.py
+++ day48/example.py
@@ -1,10 +1,14 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
 
 driver = webdriver.Chrome(options= chrome_options)
-driver.get("https://www.youtube.com")
+driver.get("https://www.amazon.com/dp/B075CYMYK6?ref_=cm_sw_r_cp_ud_ct_FM9M699VKHTT47YD50Q6&th=1")
+
+price_dollar = driver.find_element(By.CLASS_NAME, "a-price-whole")
+print(price_dollar.text)
 
 driver.close()
 driver.quit()
