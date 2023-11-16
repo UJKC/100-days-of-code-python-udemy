8c8e0deb8061f9630243b0cfbb3ff54fa26c92a0
Ujwal KC <ujwalkcsps@gmail.com>
2023-11-17 00:20:58 +0530

    working

--- day56/server.py
+++ day56/server.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return render_template('Angela.html')
+    return render_template('angela/index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
