3ae27a78a16029679571c0c97e4758fc08438272
Ujwal KC <ujwalkcsps@gmail.com>
2023-11-16 23:56:01 +0530

    rendering done

---
+++ day56/server.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# In terminal: set FLASH_APP=hello_flask.py
+
+@app.route('/')
+def hello():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
