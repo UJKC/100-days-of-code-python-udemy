1f1f1181fe5219b5d3ac88491a32c5cc464459ac
Ujwal KC <ujwalkcsps@gmail.com>
2023-11-30 15:45:24 +0530

    sample jinja

--- day57/main.py
+++ day57/main.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template
+from random import randint
+from datetime import datetime
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return "Hello World!"
+    random_number = randint(0, 11)
+    todays_year = datetime.now()
+    todays_year_year = todays_year.year
+    return render_template('index.html', num=random_number, year = todays_year_year)
 
 
 if __name__ == "__main__":
