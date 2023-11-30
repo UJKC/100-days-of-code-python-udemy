1f1bd1e39945638fb5e37f05d8ef4d4f2e7e580d
Ujwal KC <ujwalkcsps@gmail.com>
2023-11-30 16:10:53 +0530

    jinjaa

--- day57/main.py
+++ day57/main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from random import randint
 from datetime import datetime
+import requests
 
 app = Flask(__name__)
 
@@ -12,6 +13,32 @@ def home():
     todays_year_year = todays_year.year
     return render_template('index.html', num=random_number, year = todays_year_year)
 
+@app.route('/guess/<name>')
+def guesser(name):
+    # Print the name parameter first
+    print(name)
+
+    # Construct the API URL
+    url = f"https://api.genderize.io?name={name}"
+
+    # Make the API request
+    gender_data = requests.get(url=url)
+
+    # Check if the request was successful (status code 200)
+    if gender_data.status_code == 200:
+        # Parse the JSON response
+        gender_data = gender_data.json()
+
+        # Extract data from the JSON response
+        name = gender_data.get('name')
+        gender = gender_data.get('gender')
+
+        # Render the template with the obtained data
+        return render_template('guess.html', name=name, gender=gender)
+    else:
+        # Handle the case where the API request was not successful
+        print(f"API request failed with status code: {gender_data.status_code}")
+        return render_template('error.html', message='Failed to retrieve data from the API')
 
 if __name__ == "__main__":
     app.run(debug=True)
